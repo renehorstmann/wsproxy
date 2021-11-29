9b5b01b4241dd33a67594d4ce1b9c6ae9fe4015d
renehorstmann <r.horsty@gmail.com>
2021-11-29 15:26:00 +0100

    update

--- wsproxy.js
+++ wsproxy.js
@@ -2,6 +2,7 @@ let ws = require('ws');
 let net = require('net');
 
 
+const proxy_address = '127.0.0.1'
 const proxy_port = 10000;
 const ws_port = 10001;
 
@@ -9,33 +10,32 @@ let ws_server = new ws.WebSocketServer({port: ws_port});
 console.log('wsproxy listening on ws://localhost:' + ws_port + '/');
 
 let cnt = 0;
-ws_server.on('connection', function (ws_client) {
+ws_server.on('connection', ws_client => {
     cnt += 1;
     console.log('Client connected [' + cnt + ']');
 
-
-    let proxy = net.createConnection(proxy_port);
-    proxy.on('connect', function () {
+    let proxy = net.createConnection(proxy_port, proxy_address);
+    proxy.on('connect', () => {
         console.log('Connected to proxy');
 
         // websocket ws_client message callback
-        ws_client.on('message', function (message) {
+        ws_client.on('message', message => {
             console.log('ws_client send: ' + message)
             proxy.write(message)
         });
 
-        ws_client.on('close', function (close) {
+        ws_client.on('close', close => {
             console.log('ws_client closed');
             proxy.end()
         });
 
         // proxy message callback
-        proxy.on('data', function (data) {
+        proxy.on('data', data => {
             console.log('proxy send: ' + data)
             ws_client.send(data)
         });
 
-        proxy.on('close', function (had_error) {
+        proxy.on('close', had_error => {
             console.log('proxy closed');
             ws_client.close();
         });
